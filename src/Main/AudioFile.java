50e89e77241b210b51928f25d07116448840b89a
Juniormunk <theforgelover@gmail.com>
2018-07-01 02:20:36 -0400

    Add framework for java properties file

--- src/Main/AudioFile.java
+++ src/Main/AudioFile.java
@@ -1,12 +1,24 @@
 package Main;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @SuppressWarnings("serial")
 public class AudioFile extends File
 {
 	boolean selected = false;
 	int index;
+
+	public AudioFile()
+	{
+		super("");
+	}
+
+	public AudioFile(String url)
+	{
+		super(url);
+	}
 
 	public AudioFile(File file)
 	{
@@ -17,5 +29,38 @@ public class AudioFile extends File
 	{
 		return this.getName();
 	}
-	
+
+	public AudioFile(String data, String nothing)
+	{
+		super(Arrays.asList(data.substring(6, data.length() - 1).split("\\|\\|")).get(0).split("=")[1]);
+
+		data = data.substring(6, data.length() - 1);
+		ArrayList<String> arr = new ArrayList<String>();
+		arr.addAll(Arrays.asList(data.split("\\|\\|")));
+
+		selected = getBool(arr.get(1).split("=")[1]);
+		index = Integer.parseInt(arr.get(2).split("=")[1]);
+
+	}
+
+	public String toSaveString()
+	{
+		return "AudioFile [FilePath=" + this.getPath() + "|| Selected=" + selected + "|| Index=" + index + "]";
+	}
+
+	public static Boolean getBool(String bool)
+	{
+		if (bool.equals("true"))
+		{
+			return true;
+		}
+		else if (bool.equals("false"))
+		{
+			return false;
+		}
+
+		return false;
+
+	}
+
 }
--- src/Main/ConfigHandler.java
+++ src/Main/ConfigHandler.java
@@ -1,14 +1,61 @@
 package Main;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class ConfigHandler
 {
 	static Properties props;
+	static OutputStream output;
+	static FileInputStream input;
 
-	public ConfigHandler(File file)
+	public ConfigHandler(File file) throws IOException
 	{
-		
+		input = new FileInputStream(file);
+		props = new Properties();
+		props.load(input);
 	}
+
+	public void save() throws IOException
+	{
+		props.store(output, null);
+	}
+
+	// For Save
+	public String arrayToString(ArrayList<AudioFile> files)
+	{
+		ArrayList<String> outputlist = new ArrayList<String>();
+
+		for (AudioFile file : files)
+		{
+			outputlist.add(file.toString());
+		}
+
+		String output = String.join("|||", outputlist);
+
+		return output;
+	}
+
+	// For Load
+	public static ArrayList<AudioFile> stringToArrayList(String array)
+	{
+		ArrayList<AudioFile> allFiles = new ArrayList<AudioFile>();
+		ArrayList<String> allwords = new ArrayList<String>();
+
+		allwords.addAll(Arrays.asList(array.split("\\|\\|\\|")));
+
+		for (String fileString : allwords)
+		{
+			AudioFile file = new AudioFile(fileString);
+			allFiles.add(file);
+		}
+
+		return allFiles;
+	}
+
 }
